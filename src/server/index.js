0f3e0cdbea9fb59a290f3bcb6eda85b921eb28a0
Solevo1 <student.kp92@gmail.com>
2021-08-29 20:39:37 +0300

    update

--- src/server/index.js
+++ src/server/index.js
@@ -1,21 +1,56 @@
 const express = require('express');
- const favicon = require('express-favicon');
- const path = require('path');
- const port = process.env.PORT || 8080;
- 
- // здесь у нас происходит импорт пакетов и определяется порт нашего сервера
- const app = express();
- 
- //здесь наше приложение отдаёт статику
- app.use(express.static(path.join(__dirname, '../../dist/angular-project')));
- 
- //простой тест сервера
- app.get('/ping', function (req, res) {
-  return res.send('pong');
- });
- 
- //обслуживание html
- app.get('/', function (req, res) {
- res.sendFile(path.join(__dirname, '../../dist/angular-project', 'index.html'));
- });
- app.listen(port);
+const path = require('path');
+const morgan = require('morgan')
+const mongoose = require('mongoose');
+const session = require('express-session');
+const app = express();
+const port = process.env.PORT || 8080
+
+const {usersRouter} = require('./controllers/usersController'); 
+const {authRouter} = require('./controllers/authController'); 
+const {NodeCourseError} = require('./utils/errors'); 
+const { gamesRouter } = require('./controllers/gamesController');
+
+app.use(express.static(path.join(__dirname , '../../dist/angular-project')));
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname , '../../dist/angular-project/index.html'));
+});
+app.use(express.json());
+app.use(morgan('tiny'));
+
+app.use(session({
+  secret: 'secret',
+  resave: false,
+  saveUninitialized: true,
+}));
+
+app.use('/api/auth', authRouter);
+app.use('/api/users', usersRouter);
+app.use('/api/games', gamesRouter);
+app.get('*', function (req, res) {
+    res.sendFile(path.join(__dirname , '../../dist/angular-project/index.html'));
+});
+app.use((req, res, next) => {
+    res.status(404).json({message: 'Not found'})
+});
+
+app.use((err, req, res, next) => {
+    if (err instanceof NodeCourseError) {
+        return res.status(err.status).json({message: err.message});
+    }
+    res.status(500).json({message: err.message});
+});
+
+
+const start = async () => {
+    try {
+        await mongoose.connect('mongodb+srv://testuser:1111@cluster0.cxpqy.mongodb.net/data?retryWrites=true&w=majority', {
+            useNewUrlParser: true, useUnifiedTopology: true
+        });
+        app.listen(8080);
+    } catch (err) {
+        console.error(`Error on server startup: ${err.message}`);
+    }
+}
+
+start();
