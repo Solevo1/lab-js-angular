e0886d9034887272fc81875e61b6fdee138f46af
Solevo1 <student.kp92@gmail.com>
2021-08-29 20:24:16 +0300

    update

--- src/server/index.js
+++ src/server/index.js
@@ -5,39 +5,13 @@ const mongoose = require('mongoose');
 const session = require('express-session');
 const app = express();
 
-const {usersRouter} = require('./controllers/usersController'); 
-const {authRouter} = require('./controllers/authController'); 
-const {NodeCourseError} = require('./utils/errors'); 
-const { gamesRouter } = require('./controllers/gamesController');
 
 app.use(express.static(path.join(__dirname , '../dist/angular-project')));
 app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname , '../dist/angular-project/index.html'));
 });
-app.use(express.json());
-app.use(morgan('tiny'));
-
-app.use(session({
-  secret: 'secret',
-  resave: false,
-  saveUninitialized: true,
-}));
-
-app.use('/api/auth', authRouter);
-app.use('/api/users', usersRouter);
-app.use('/api/games', gamesRouter);
 app.get('*', function (req, res) {
     res.sendFile(path.join(__dirname , '../dist/angular-project/index.html'));
-});
-app.use((req, res, next) => {
-    res.status(404).json({message: 'Not found'})
-});
-
-app.use((err, req, res, next) => {
-    if (err instanceof NodeCourseError) {
-        return res.status(err.status).json({message: err.message});
-    }
-    res.status(500).json({message: err.message});
 });
 
 
