d53e54ef81f063910ab57da58f998618bb0ceba0
Solevo1 <student.kp92@gmail.com>
2021-08-29 20:29:25 +0300

    update

--- src/server/index.js
+++ src/server/index.js
@@ -4,6 +4,7 @@ const morgan = require('morgan')
 const mongoose = require('mongoose');
 const session = require('express-session');
 const app = express();
+ const port = process.env.PORT || 8080;
 
 
 app.use(express.static(path.join(__dirname , '../dist/angular-project')));
@@ -20,7 +21,7 @@ const start = async () => {
         await mongoose.connect('mongodb+srv://testuser:1111@cluster0.cxpqy.mongodb.net/data?retryWrites=true&w=majority', {
             useNewUrlParser: true, useUnifiedTopology: true
         });
-        app.listen(8080);
+        app.listen(port);
     } catch (err) {
         console.error(`Error on server startup: ${err.message}`);
     }
